efca3eee7c761dbf73cc818683f16878cfbd464b
Nick-voz <nik.vozisow@gmail.com>
2026-09-16 20:39:03 +0300

    awesome: fix superproductivity class name, remove wlctl from floats

--- awesome/.config/awesome/rules.lua
+++ awesome/.config/awesome/rules.lua
@@ -55,7 +55,7 @@ awful.rules.rules = {
 	{ rule = { class = "steam" }, properties = { screen = 1, tag = "4" } },
 
 	{ rule = { name = "obsidian" }, properties = { screen = 1, tag = "6" } },
-	{ rule = { class = "Super Productivity" }, properties = { screen = 1, tag = "6" } },
+	{ rule = { class = "superproductivity" }, properties = { screen = 1, tag = "6" } },
 
 	{
 		rule_any = { class = { "libreoffice", "libreoffice-writer", "Soffice", "soffice" } },
@@ -71,7 +71,6 @@ awful.rules.rules = {
 		rule_any = {
 			class = {
 				"pulsemixer",
-				"wlctl",
 				"Nm-connection-editor",
 				"Network Connections",
 				"Nm-applet",
